a32bd15038835baede33fa4087aacfa100267af7
sanchikgn <kasumisanchika@gmail.com>
2017-10-09 15:59:10 +0530

    First Commit

---
+++ app/src/main/java/com/example/crosswalk/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.crosswalk;
+
+//import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import org.xwalk.core.XWalkActivity;
+import org.xwalk.core.XWalkPreferences;
+import org.xwalk.core.XWalkView;
+
+public class MainActivity extends XWalkActivity {
+    private XWalkView xWalkWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        xWalkWebView=(XWalkView)findViewById(R.id.xwalkWebView);
+        xWalkWebView.load("https://192.168.8.114:8080/cam", null);
+        // turn on debugging
+        XWalkPreferences.setValue(XWalkPreferences.REMOTE_DEBUGGING, true);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (xWalkWebView != null) {
+            xWalkWebView.pauseTimers();
+            xWalkWebView.onHide();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (xWalkWebView != null) {
+            xWalkWebView.resumeTimers();
+            xWalkWebView.onShow();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (xWalkWebView != null) {
+            xWalkWebView.onDestroy();
+        }
+    }
+
+}
